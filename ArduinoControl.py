75921c6bcf8ade87515858f0a4f335ba46d1d83d
Jonatan Wästlund <jonte.w1@gmail.com>
2017-03-27 20:21:35 +0200

    Update ArduinoControl.py

--- ArduinoControl.py
+++ ArduinoControl.py
@@ -1,8 +1,21 @@
 from nanpy import ArduinoApi, SerialManager
+import laserProgram
 
-uSonicConnection = SerialManager(device='ultrasonic arduino')
+laserConnection = SerialManager(device='laser arduino')
 gripperConnection = SerialManager(device='gripper arduino')
 
-uSonicA = ArduinoApi(connection=uSonicConnection)
-gripperA = ArduinoApi(connection=gripperConnection)
+laserA = ArduinoApi(connection=laserConnection)
+motorA = ArduinoApi(connection=motorConnection)
+
+laserL = A5
+laserR = A4
+laserF = A3
+laserB = A2
+
+
+
+laserA.pinMode(laserL, INPUT)
+laserA.pinMode(laserR, INPUT)
+laserA.pinMode(laserF, INPUT)
+laserA.pinMode(laserB, INPUT)
 
