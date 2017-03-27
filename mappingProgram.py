1e358c90812b091a7f052c1d79505862a929c1da
Jonatan Wästlund <jonte.w1@gmail.com>
2017-03-27 22:16:15 +0200

    Update mappingProgram.py

--- mappingProgram.py
+++ mappingProgram.py
@@ -1,1 +1,102 @@
+from sensorProgram import *
+from arduinoControl import *
 
+x = 0
+y = 0
+d = 0
+
+notDiscovered = 0
+discovered = 1
+wall = 3
+noWall = 4
+blackGround = 5
+heatedTile = 6
+letterTile = 7
+start = 8
+
+def mapping():
+	if d == 0:
+    	if laserValue('L') > 10:
+			map[str(x+1) + ',' + str(y)] = noWall
+		else:
+      		map[str(x+1) + ',' + str(y)] = wall
+			
+		if laserValue('R') > 10:
+			map[str(x+1) + ',' + str(y)] = noWall
+		else:
+			map[str(x+1) + ',' + str(y)] = wall
+			
+		if laserValue('F') > 10:
+			map[str(x) + ',' + str(y+1)] = noWall
+		else:
+			map[str(x) + ',' + str(y+1)] = wall
+			
+		if laserValue('B') > 10:
+			map[str(x) + ',' + str(y-1)] = noWall
+		else:
+			map[str(x) + ',' + str(y-1)] = wall
+			
+  	elif d == 1:
+    	if laserValue('L') > 10:
+			map[str(x) + ',' + str(y+1)] = noWall
+		else:
+      		map[str(x+1) + ',' + str(y+1)] = wall
+			
+		if laserValue('R') > 10:
+			map[str(x) + ',' + str(y-1)] = noWall
+		else:
+			map[str(x) + ',' + str(y-1)] = wall
+			
+		if laserValue('F') > 10:
+			map[str(x+1) + ',' + str(y)] = noWall
+		else:
+			map[str(x+1) + ',' + str(y)] = wall
+			
+		if laserValue('B') > 10:
+			map[str(x-1) + ',' + str(y)] = noWall
+		else:
+			map[str(x-1) + ',' + str(y)] = wall
+    
+  	elif d == 2:
+    	if laserValue('L') > 10:
+			map[str(x+1) + ',' + str(y)] = noWall
+		else:
+      		map[str(x+1) + ',' + str(y)] = wall
+			
+		if laserValue('R') > 10:
+			map[str(x+1) + ',' + str(y)] = noWall
+		else:
+			map[str(x+1) + ',' + str(y)] = wall
+			
+		if laserValue('F') > 10:
+			map[str(x) + ',' + str(y+1)] = noWall
+		else:
+			map[str(x) + ',' + str(y+1)] = wall
+			
+		if laserValue('B') > 10:
+			map[str(x) + ',' + str(y-1)] = noWall
+		else:
+			map[str(x) + ',' + str(y-1)] = wall
+		
+  	elif d == 3:
+    	if laserValue('L') > 10:
+			map[str(x+1) + ',' + str(y)] = noWall
+		else:
+      		map[str(x+1) + ',' + str(y)] = wall
+			
+		if laserValue('R') > 10:
+			map[str(x+1) + ',' + str(y)] = noWall
+		else:
+			map[str(x+1) + ',' + str(y)] = wall
+			
+		if laserValue('F') > 10:
+			map[str(x) + ',' + str(y+1)] = noWall
+		else:
+			map[str(x) + ',' + str(y+1)] = wall
+			
+		if laserValue('B') > 10:
+			map[str(x) + ',' + str(y-1)] = noWall
+		else:
+			map[str(x) + ',' + str(y-1)] = wall
+    
+    
