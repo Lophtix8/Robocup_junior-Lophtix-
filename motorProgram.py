8515c408a987e34ae9849f56ed238fb35c0b1b00
Jonatan Wästlund <jonte.w1@gmail.com>
2017-03-28 17:35:28 +0200

    Update motorProgram.py

--- motorProgram.py
+++ motorProgram.py
@@ -1,5 +1,5 @@
 from nanpy import (ArduinoApi, SerialManager)
 
 motorConnection = SerialManager(device=/dev/ttyUSB0)
-A = ArduinoApi(connection='motorConnection')
+A = ArduinoApi(connection=motorConnection)
 
