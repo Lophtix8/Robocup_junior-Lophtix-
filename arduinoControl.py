e9709cdb9304cc0920f98f5b12a73a81026eeddd
Jonatan Wästlund <jonte.w1@gmail.com>
2017-03-31 17:12:53 +0200

    Update arduinoControl.py

--- arduinoControl.py
+++ arduinoControl.py
@@ -1,6 +1,11 @@
+from nanpy import (ArduinoApi, SerialManager)
+import RPi.GPIO as GPIO
 from motorProgram import *
 from sensorProgram import *
 from mappingProgram import *
+
+connection = SerialManager(device='/dev/ttyUSB0')
+a = ArduinoApi(connection=connection)
 
 map = {x + ',' + y: start}
 
