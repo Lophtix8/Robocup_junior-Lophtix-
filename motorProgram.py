ca7f85bfbbe6cd9b96e4724b78b35181f84bfbc9
Jonatan Wästlund <jonte.w1@gmail.com>
2017-03-31 18:34:06 +0200

    Update motorProgram.py

--- motorProgram.py
+++ motorProgram.py
@@ -1,5 +1,22 @@
 from nanpy import (ArduinoApi, SerialManager)
+import time
 
 motorConnection = SerialManager(device=/dev/ttyUSB0)
 A = ArduinoApi(connection=motorConnection)
+
+mLeft1 = 'input1'
+mLeft2 = 'input2'
+eLeft = 'Enable1'
+
+mRight1 = 'input3'
+mRight2 = 'input4'
+eRight = 'Enable2'
+
+A.pinMode(mLeft1, A.OUTPUT)
+A.pinMode(mLeft2, A.OUTPUT)
+A.pinMode(eLeft2, A.OUTPUT)
+
+A.pinMode(mRight1, A.OUTPUT)
+A.pinMode(mRight2, A.OUTPUT)
+A.pinMode(eRight, A.OUTPUT)
 
