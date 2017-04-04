49f292e9b6c68777ac6664185c50707d5b503861
Jonatan Wästlund <jonte.w1@gmail.com>
2017-04-04 14:56:53 +0200

    Update mappingProgram.py

--- mappingProgram.py
+++ mappingProgram.py
@@ -6,39 +6,39 @@ x = 0
 y = 0
 d = 0
 
-notDiscovered = 0
-discovered = 1
-wall = 3
-noWall = 4
-blackGround = 5
-heatedTile = 6
-letterTile = 7
-start = 8
+notDiscovered = []
+discovered = []
+wall = []
+noWall = []
+blackGround = []
+heatedTile = []
+letterTile = []
+start = []
 
 def Mapping():
 	##### MAPPING THE ROOM ######################################################
 	if d == 0:
-		if (laserValue('L') > 10) and (map[str(x-1) + ',' + str(y)] == None):
-			map[str(x-1) + ',' + str(y)] = noWall
-		elif map[str(x-1) + ',' + str(y)] == None:
-			map[str(x-1) + ',' + str(y)] = wall
+		if (laserValue('L') > 10) and ([x-1, y] in (wall or noWall) == False):
+			noWall.append([x-1, y])
+		elif [x-1, y] in (wall or noWall) == False:
+			wall.append([x-1, y])
 			
-		if (laserValue('R') > 10) and (map[str(x+1) + ',' + str(y)] == None):
-			map[str(x+1) + ',' + str(y)] = noWall
-		elif map[str(x+1) + ',' + str(y)] == None:
-			map[str(x+1) + ',' + str(y)] = wall
+		if (laserValue('R') > 10) and ([x+1, y] in (wall or noWall) == False):
+			noWall.append([x+1, y])
+		elif [x+1, y] in (wall or noWall) == False:
+			wall.append([x+1, y])
 			
-		if (laserValue('F') > 10) and (map[str(x) + ',' + str(y+1)] == None):
-			map[str(x) + ',' + str(y+1)] = noWall
-		elif map[str(x) + ',' + str(y+1)] == None:
-			map[str(x) + ',' + str(y+1)] = wall
+		if (laserValue('F') > 10) and ([x, y+1] in (wall or noWall) == False):
+			noWall.append([x, y+1])
+		elif [x, y+1] in (wall or noWall) == False:
+			wall.append([x, y+1])
 			
-		if (laserValue('B') > 10) and (map[str(x) + ',' + str(y-1)] == None):
-			map[str(x) + ',' + str(y-1)] = noWall
-		elif map[str(x) + ',' + str(y-1)] == None:
-			map[str(x) + ',' + str(y-1)] = wall
+		if (laserValue('B') > 10) and ([x, y-1] in (wall or noWall) == False):
+			noWall.append([x, y-1])
+		elif [x, y-1] in (wall or noWall) == False:
+			wall.append([x, y-1])
 			
-		##### DECIDING DIRECTION TO TURN########################################
+		##### DECIDING DIRECTION TO TURN ########################################
 		if (map[str(x-2) + ',' + str(y)] == None) and (map[str(x-1) + ',' + str(y) == noWall]):
 			x = x-2
 			d = 3
