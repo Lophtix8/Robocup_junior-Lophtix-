5d646c5e7851af1e4085cbf038c51b67b627af32
Jonatan Wästlund <jonte.w1@gmail.com>
2017-04-04 19:47:43 +0200

    Update mappingProgram.py

--- mappingProgram.py
+++ mappingProgram.py
@@ -75,97 +75,111 @@ def Mapping():
 			wall.append([x, y+1])
 			
 		if (laserValue('R') > 10) and ([x, y-1] in wall or noWall):
-			map[str(x) + ',' + str(y-1)] = noWall
-		elif map[str(x) + ',' + str(y-1)] == None:
-			map[str(x) + ',' + str(y-1)] = wall
+			noWall.append([x, y-1])
+			notDiscovered.append([x, y-2])
+		elif [x, y-1] in wall or noWall:
+			wall.append([x, y-1])
 			
-		if (laserValue('F') > 10) and (map[str(x+1) + ',' + str(y)] == None):
-			map[str(x+1) + ',' + str(y)] = noWall
-		elif map[str(x+1) + ',' + str(y)] == None:
-			map[str(x+1) + ',' + str(y)] = wall
+		if (laserValue('F') > 10) and ([x+1, y] in wall or noWall):
+			noWall.append[x+1, y]
+			notDiscovered.append([x+2, y])
+		elif [x+1, y] in wall or noWall:
+			wall.append([x+1, y])
 			
-		if (laserValue('B') > 10) and (map[str(x-1) + ',' + str(y)] == None):
-			map[str(x-1) + ',' + str(y)] = noWall
-		elif map[str(x-1) + ',' + str(y)] == None:
-			map[str(x-1) + ',' + str(y)] = wall
+		if (laserValue('B') > 10) and ([x-1 ,y] in wall or noWall):
+			noWall.append([x-1, y])
+			notDiscovered.append([x-2, y])
+		elif [x-1, y] in wall or noWall:
+			wall.append([x-1, y])
 			
 		##### DECIDING DIRECTION TO TURN########################################
-		if (map[str(x) + ',' + str(y+2)] == None) and (map[str(x) + ',' + str(y+1)] == noWall):
+		if ([x, y+2] in notDiscovered) and ([x, y+1] in noWall):
 			y = y+2
 			d = 0
 			turnLeft()
 		
-		elif (map[str(x) + ',' + str(y-2)] == None) and (map[str(x) + ',' + str(y-1)] == noWall):
+		elif ([x, y-2] in notDiscovered) and ([x, y-1] in noWall):
 			y = y-2
 			d = 2
 			turnRight()
 		
-		elif (map[str(x+2) + ',' + str(y)] == None) and (map[str(x+1) + ',' + str(y)] == noWall):
+		elif ([x+2, y] in notDiscovered) and ([x+1, y] in noWall):
 			x = x+2
 			d = 1
 			turnForward()
 			
-		elif (map[str(x-2) + ',' + str(y)] == None) and (map[str(x-1) + ',' + str(y)] == noWall):
+		elif ([x, y-2] in notDiscovered) and ([x, y-1] in noWall):
 			x = x-2
 			d = 3
 			turnBackwards()
     	
 	elif d == 2:
-		if (laserValue('L') > 10) and (map[str(x+1) + ',' + str(y)] == None):
-			map[str(x+1) + ',' + str(y)] = noWall
-		elif map[str(x+1) + ',' + str(y)] == None:
-			map[str(x+1) + ',' + str(y)] = wall
+		if (laserValue('L') > 10) and ([x+1, y] in wall or noWall):
+			noWall.append([x+1, y])
+			if not [x+1, y] in discovered:
+				notDiscovered.append([x+2, y])
+		elif [x+1, y] in wall or noWall:
+			wall.append([x+1, y])
 			
-		if (laserValue('R') > 10) and (map[str(x-1) + ',' + str(y)] == None):
-			map[str(x-1) + ',' + str(y)] = noWall
-		elif (map[str(x-1) + ',' + str(y)] == None):
-			map[str(x-1) + ',' + str(y)] = wall
+		if (laserValue('R') > 10) and ([x-1, y] in wall or noWall):
+			noWall.append([x-1, y])
+			if not [x-1, y] in discovered:
+				notDiscovered.append([x-2, y])
+		elif [x-1, y] in wall or noWall:
+			wall.append([x-1, y])
 			
-		if (laserValue('F') > 10) and (map[str(x) + ',' + str(y-1)] == None):
-			map[str(x) + ',' + str(y-1)] = noWall
-		elif map[str(x) + ',' + str(y-1)] == None:
-			map[str(x) + ',' + str(y-1)] = wall
+		if (laserValue('F') > 10) and ([x, y-1] in wall or noWall):
+			noWall.append([x, y-1])
+			notDiscovered.append([x, y-2])
+		elif [x, y-1] in wall or noWall:
+			wall.append([x, y-1])
 			
-		if (laserValue('B') > 10) and (map[str(x) + ',' + str(y+1)] == None):
-			map[str(x) + ',' + str(y+1)] = noWall
-		elif map[str(x) + ',' + str(y+1)] == None:
-			map[str(x) + ',' + str(y+1)] = wall
+		if (laserValue('B') > 10) and ([x, y+1] in wall or noWall):
+			noWall.append([x, y+1])
+		elif [x, y+1] in wall or noWall:
+			wall.append([x, y+1])
 			
-		if (map[str(x+2) + ',' + str(y)] == None) and (map[str(x+1) + ',' + str(y)] == noWall):
+		if ([x+2, y] in notDiscovered) and ([x+2, y] in noWall):
 			x = x+2
 			d = 1
 			turnLeft()
 			
-		elif (map[str(x-2) + ',' + str(y)] == None) and (map[str(x-1) + ',' + str(y)] == noWall):
+		elif ([x-2, y] in notDiscovered) and ([x-1, y] in noWall):
 			x = x-2
 			d = 3
 			turnRight()
 			
-		elif (map[str(x) + ',' + str(y-2)] == None) and (map[str(x) + ',' + str(y-1)] == noWall):
+		elif ([x, y-2] in notDiscovered) and ([x, y-1] in noWall):
 			y = y-2
 			d = 2
 			turnForward()
 		
-		elif (map[str(x) + ',' + str(y+2)] == None) and (map[str(x) + ',' + str(y+1)] == noWall):
+		elif ([x, y+2] in notDiscovered) and ([x, y+1] in noWall):
 			y = y+2
 			d = 0
 			turnBackwards()
 		
 	elif d == 3:
-		if (laserValue('L') > 10) and (map[str(x) + ',' + str(y-1)] == None):
-			map[str(x) + ',' + str(y-1)] = noWall
-		elif map[str(x) + ',' + str(y-1)] == None:
-			map[str(x) + ',' + str(y-1)] = wall
+		if (laserValue('L') > 10) and ([x, y-1] in wall or noWall):
+			noWall.append([x, y-1])
+			if not [x, y-2] in discovered:
+				notDiscovered.append([x, y-2])
+		elif [x, y-1] in wall or noWall:
+			wall.append([x, y-1])
 			
-		if (laserValue('R') > 10) and (map[str(x) + ',' + str(y+1)] == None):
-			map[str(x) + ',' + str(y+1)] = noWall
-		elif map[str(x) + ',' + str(y+1)] == None:
-			map[str(x) + ',' + str(y+1)] = wall
+		if (laserValue('R') > 10) and ([x, y+1] in wall or noWall):
+			noWall.append([x, y+1])
+			if not [x, y+2] in discovered:
+				notDiscovered.append([x, y+2])
+		elif [x, y+1] in wall or noWall:
+			wall.append([x, y+1])
 			
-		if (laserValue('F') > 10) and (map[str(x-1) + ',' + str(y)] == None):
-			map[str(x-1) + ',' + str(y)] = noWall
-		elif map[str(x-1) + ',' + str(y)] == None:
-			map[str(x-1) + ',' + str(y)] = wall
+		if (laserValue('F') > 10) and ([x-1, y] in wall or noWall):
+			noWall.append([x-1, y])
+			if not [x-2, y] in discovered:
+				notDiscovered.append([x-2, y])
+		elif [x-1, y] in wall or noWall:
+			wall.append([x-1, y])
 			
 		if (laserValue('B') > 10) and (map[str(x+1) + ',' + str(y)] == None):
 			map[str(x+1) + ',' + str(y)] = noWall
