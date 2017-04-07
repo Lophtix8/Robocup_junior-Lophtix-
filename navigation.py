0c8b7164a8a57cb6969cb2161631e61f624a9250
Jonatan Wästlund <jonte.w1@gmail.com>
2017-04-07 09:49:16 +0200

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -1,16 +1,16 @@
 class Navigation:
 	def closestCross():
-		for a in range(0, len(cross)):
-			Value = cross[a][0] - x
-			cross[a] = [cross[a][0], cross[a][1], abs(Value)]
+		for a in range(0, len(crosses)):
+			Value = crosses[a][0] - x
+			crosses[a] = [crosses[a][0], crosses[a][1], abs(Value)]
 		
-		cross.sort(key = lambda x: int(x[2]))
+		crosses.sort(key = lambda x: int(x[2]))
 		found = False
 		a = 0
 		while found == False:
-			if checkPossibility(cross[a][0], cross[a][1], cross[a][2])
+			if checkPossibility(crosses[a][0], crosses[a][1], crosses[a][2])
 				found = True
-				drive(cross[a][0], cross[a][1])
+				drive(crosses[a][0], crosses[a][1])
 				
-			else:
+			else:fec
 				a = a+1
