4689109882655d96c25a784197f6edf728db2fdd
Jonatan Wästlund <jonte.w1@gmail.com>
2017-04-07 13:34:55 +0200

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -9,24 +9,30 @@ class Navigation:
 		found = False
 		a = 0
 		while found == False:
-			if checkPossibility(crosses[a][0], crosses[a][1], Value, x, 0)
+			if checkPossibility(crosses[a][0], crosses[a][1], Value, x, 0):
 				found = True
 				
 			else:
-				a = a+1
+				a = a+1 
 
 	def checkPossibility(a, b, c, d, e):
 		if c == 0:
 			return False
 		
-		if c > 0:
+		if c > 0 and e == 0:
 			result = True
-			for f in range(d+1, abs(c)):
-				if [f, y] in noWall
-					
-				
-				else:
+			for f in range(d+1, c):
+				if ([f, y] not in noWall) and ([f, y] in wall):
 					result = False
 			
 			if result = True:
+				return True
+			
+		if c < 0 and e == 0:
+			result = True
+			for f in range(d-1, c, -1):
+				if ([f, y] not in noWall) and ([f, y] in wall):
+					result = False
+			
+			if result = True:ö.
 				return True
