d608bfcfe8ae7969bc22c01b775937428cd15000
Jonatan Wästlund <jonte.w1@gmail.com>
2017-07-18 22:52:49 +0200

    Update Ai-navigation.py

--- Ai-navigation.py
+++ Ai-navigation.py
@@ -1,1 +1,11 @@
+import random as random
 
+map = {}
+
+def mapCreator():
+  for n in range(20, 2)
+    for n2 in range(20, 2)
+      tileValue = randint(0, 1)
+      map[str(n) + ',' + str(n2)] = tileValue
+
+print(map)
